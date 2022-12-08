b76b6394107f86e4f325819454ed15f763ddc8f8
Skyreth <63458729+Skyreth@users.noreply.github.com>
2022-12-08 05:33:24 +0100

    Update ItemExample.lua

--- ItemExample.lua
+++ ItemExample.lua
@@ -5,10 +5,6 @@ local player = Players.LocalPlayer
 local rs = game:GetService("ReplicatedStorage")
 local Inventory = require(rs.InventoryManager)
 
-local character = player.CharacterAdded:Wait()
-local humanoid = character:WaitForChild("Humanoid")
-local Animator = humanoid:WaitForChild("Animator")
-
 local function PickUp()
 	Inventory.PickUpItem(player, tool, Inventory.getFirstSlot(player.PlayerGui.GameUi.Inventory:GetChildren()), player.PlayerGui.GameUi.Inventory, Inventory)
 end
